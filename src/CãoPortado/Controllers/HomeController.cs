d6b454eac63ef418e8e33feb815b595354e58df5
Bella Damião <belladamiao2@gmail.com>
2022-05-26 19:19:29 -0300

    Commit - Bella: - Página A Empresa(faltando arrumar carrossel) - Seta tela cad user apagada - Registre-se (Login) sem capslock - Home ao invés de Index na navbar

--- src/CãoPortado/Controllers/HomeController.cs
+++ src/CãoPortado/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace PetHotel.Controllers
             return View();
         }
 
+        public IActionResult Empresa()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
